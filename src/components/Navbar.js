133e4332b5db09f502de47af67ad2b4ad3500c7c
KumarAtmish <73941481+KumarAtmish@users.noreply.github.com>
2020-12-18 19:45:09 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWarapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY3YIvtw3oQhaqOyj4vKanlf8LLaa1p0hAdA&usqp=CAU"
@@ -22,13 +24,25 @@ class Navbar extends React.Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
             my cart
-          </button>
+          </ButtonContainer>
         </Link>
-      </div>
+      </NavWarapper>
     );
   }
 }
+
+const NavWarapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainBlue) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize !important;
+  }
+`;
+
 export default Navbar;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Product from "./Product";
+import Title from "./Title";
 
 class ProductList extends React.Component {
+  state = {
+    products: []
+  };
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <Product />
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 }
